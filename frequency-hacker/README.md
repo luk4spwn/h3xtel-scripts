53da9b023b472ad2272227822e59da2f0e0a8af1
luk4spwn <164707110+luk4spwn@users.noreply.github.com>
2024-05-07 13:30:36 -0400

    Add files via upload

---
+++ frequency-hacker/README.md
@@ -0,0 +1,12 @@
+test
+
+```
+
+Title: Frecuency Hacker
+
+Description: Can you crack this substitution cipher? ;)
+Author: D-Cryp7
+
+Encrypted flag: 'hppdlidt tuyxhf da tyzlicrynlwz iwhz fwcupe kh lyhiiz fdqlph ydrwi da n yhthai lnlhy n rycul cv lwzfdtdfif ni tnqkyderh uadxhyfdiz lyhfhaihe na hsnqlph cv na hppdlidt tuyxh da jwdtw n tyzlicrynlwdt clhynicy inbhf n fhi cv auqkhyf daic nttcuai iwha ufhf iwcfh auqkhyf ic lycxh iwni iwh ahj bhz wnf khha lyceuthe kui iwdf df aci na hsnqlph cv iwh lhyvhti nprcydiwq iwni fcpxhf hxhyz tcathdxnkph lyckphq hflhtdnppz aci vcy hatyzlidca khtnufh di phnxhf cui cah fqnpp lnyi iwh qniwhqnidtnp vcyqupnidca iwni df ntiunppz ufhe ic rhahynih iwh yhfupi iwh lyckphq df iwni iwh nprcydiwq df aci ntiunppz n lhyvhti nprcydiwq di echfai ahthffnydpz ufh iwh ydrwi fhi cv auqkhyf kui iwh nafjhy df iwni iwhyh nyh ni phnfi fhxhynp jnzf ic fcpxh iwni vuatidca cah cv iwcfh ufhf tna kh ehydxhe vycq iwh aniuynp auqkhyf iwhqfhpxhf jwhyh zcu wnxh n fhmuhath cv auqkhyf iwni cah fhhf nf wnxdar qcyh iwna cah xnpuh wh hslpndaheiwh lyckphq df iwni capz thyinda fcpuidcaf ufdar naz cv iwh edvvhyhai fzqkcpf tna ntiunppz lyceuth iwh tcyyhti enin iwni qhnaf iwni dif dqlcffdkph ic tcqh ul jdiw lhyvhti auqkhyf da rhahynp jwha iwh tcyyhti nafjhy df n tcqlphihpz nykdiynyz fhi cv auqkhyfn lyckphq pdbh iwdf df aci gufi n qniwhqnidtnp cah nae iwhyh df n twnath iwni qcfi tyzlicrynlwdt vuatidcaf jdiw qupidlph auqkhyf cv clhynidcaf futw nf iwh yfn nprcydiwq tcupe kh dqlycxhe jdiw iwhdy nprcydiwqf kz fdqlpz twnardar iwh auqkhyf cv clhynidca da iwh hae di jcupe qnbh fhafh ic phnxh iwh qniwhqnidtf nae lyckphqf cv jwz iwdf df iyuh aci khdar edftuffhe'
+
+```
---
+++ frequency-hacker/script.py
@@ -0,0 +1,28 @@
+ct = "hppdlidt tuyxhf da tyzlicrynlwz iwhz fwcupe kh lyhiiz fdqlph ydrwi da n yhthai lnlhy n rycul cv lwzfdtdfif ni tnqkyderh uadxhyfdiz lyhfhaihe na hsnqlph cv na hppdlidt tuyxh da jwdtw n tyzlicrynlwdt clhynicy inbhf n fhi cv auqkhyf daic nttcuai iwha ufhf iwcfh auqkhyf ic lycxh iwni iwh ahj bhz wnf khha lyceuthe kui iwdf df aci na hsnqlph cv iwh lhyvhti nprcydiwq iwni fcpxhf hxhyz tcathdxnkph lyckphq hflhtdnppz aci vcy hatyzlidca khtnufh di phnxhf cui cah fqnpp lnyi iwh qniwhqnidtnp vcyqupnidca iwni df ntiunppz ufhe ic rhahynih iwh yhfupi iwh lyckphq df iwni iwh nprcydiwq df aci ntiunppz n lhyvhti nprcydiwq di echfai ahthffnydpz ufh iwh ydrwi fhi cv auqkhyf kui iwh nafjhy df iwni iwhyh nyh ni phnfi fhxhynp jnzf ic fcpxh iwni vuatidca cah cv iwcfh ufhf tna kh ehydxhe vycq iwh aniuynp auqkhyf iwhqfhpxhf jwhyh zcu wnxh n fhmuhath cv auqkhyf iwni cah fhhf nf wnxdar qcyh iwna cah xnpuh wh hslpndaheiwh lyckphq df iwni capz thyinda fcpuidcaf ufdar naz cv iwh edvvhyhai fzqkcpf tna ntiunppz lyceuth iwh tcyyhti enin iwni qhnaf iwni dif dqlcffdkph ic tcqh ul jdiw lhyvhti auqkhyf da rhahynp jwha iwh tcyyhti nafjhy df n tcqlphihpz nykdiynyz fhi cv auqkhyfn lyckphq pdbh iwdf df aci gufi n qniwhqnidtnp cah nae iwhyh df n twnath iwni qcfi tyzlicrynlwdt vuatidcaf jdiw qupidlph auqkhyf cv clhynidcaf futw nf iwh yfn nprcydiwq tcupe kh dqlycxhe jdiw iwhdy nprcydiwqf kz fdqlpz twnardar iwh auqkhyf cv clhynidca da iwh hae di jcupe qnbh fhafh ic phnxh iwh qniwhqnidtf nae lyckphqf cv jwz iwdf df iyuh aci khdar edftuffhe"
+
+ct = ct.replace("h", "E")
+ct = ct.replace("i", "T")
+ct = ct.replace("w", "H")
+ct = ct.replace("n", "A")
+ct = ct.replace("a", "N")
+ct = ct.replace("e", "D")
+ct = ct.replace("q", "M")
+ct = ct.replace("d", "I")
+ct = ct.replace("t", "C")
+ct = ct.replace("p", "L")
+ct = ct.replace("y", "R")
+ct = ct.replace("x", "V")
+ct = ct.replace("f", "S")
+ct = ct.replace("r", "G")
+ct = ct.replace("c", "O")
+ct = ct.replace("u", "U")
+ct = ct.replace("k", "B")
+ct = ct.replace("j", "W")
+ct = ct.replace("z", "Y")
+ct = ct.replace("l", "P")
+ct = ct.replace("v", "F")
+ct = ct.replace("s", "X")
+ct = ct.replace("m", "C")
+
+
+print(ct)
